3a37a185eb5474274acfe495b7dd16c80469b22e
Pranjal Agrawal <pranshu.eth@gmail.com>
2024-12-06 19:49:19 +0530

    Add ABIs

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,3 +1,5 @@
+import pool from "./abis/OpinionPool.json";
+import * as chains from "viem/chains";
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
 /**
@@ -11,6 +13,13 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *   },
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+  [chains.sepolia.id]: {
+    POOL_CONTRACT: {
+      address: "0x8aDb7Ca8ba24a1fDCea74fED0a32F158d6d82Ad7",
+      abi: pool.abi as Array<any>,
+    },
+  },
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
