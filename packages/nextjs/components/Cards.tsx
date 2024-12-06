955a091124fe98b08b22c7cdf1bde2d1a928e842
blprohit <rohitblpprajapat@gmail.com>
2024-12-07 03:52:07 +0530

    UI change

--- packages/nextjs/components/Cards.tsx
+++ packages/nextjs/components/Cards.tsx
@@ -26,13 +26,13 @@ const Card: React.FC<CardProps> = ({ title, description, betoption, betoption2 }
 
   return (
     <>
-      <div className="px-5 mx-4 my-4 bg-violet-950 text-violet-300 ease-in-out transition duration-300 flex flex-col justify-between w-80 h-[18vh] border border-violet-950 rounded-2xl hover:shadow-xl hover:shadow-violet-400 hover:scale-105">
+      <div className="px-5 mx-4 my-4 bg-primary text-accent ease-in-out transition duration-300 flex flex-col justify-between w-80 h-[18vh] border border-violet-950 rounded-2xl hover:shadow-xl hover:shadow-secondary hover:scale-105">
         <div className="mx-2 mt-2">
           <h2 className="font-bold ">{title}</h2>
           <p>{description}</p>
         </div>
-        <div className="flex justify-between px-5 py-2">
-          <button onClick={handleButtonClick} className="px-5 py-2 rounded-xl bg-green-500 text-white hover:scale-105">
+        <div className="flex justify-between px-3 py-2">
+          <button onClick={handleButtonClick} className="px-4 py-2 rounded-xl bg-green-500 text-white hover:scale-105">
             {betoption}
           </button>
           <button onClick={handleButtonClick} className="px-5 py-2 rounded-xl bg-red-500 text-white hover:scale-105">
@@ -43,14 +43,21 @@ const Card: React.FC<CardProps> = ({ title, description, betoption, betoption2 }
       {showPopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end">
           <div
-            className="bg-white p-5 rounded-t-3xl w-full h-[50vh]"
+            className="bg-primary p-5 rounded-t-3xl w-full h-[50vh]"
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
           >
-            <h2>Popup Content</h2>
-            <button onClick={() => setShowPopup(false)} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
-              Close
-            </button>
+            <div className="flex justify-between items-center">
+              <h2>{title}</h2>
+              <button onClick={() => setShowPopup(false)} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+                Close
+              </button>
+            </div>
+            <p>{description}</p>
+            <div className="flex justify-between items-center mx-10">
+              <button className="px-10 py-2 bg-secondary rounded-lg">Sell</button>
+              <button className="px-10 py-2 bg-secondary rounded-lg">Buy</button>
+            </div>
           </div>
         </div>
       )}
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,10 +4,10 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SwitchTheme } from "./SwitchTheme";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
-import { SwitchTheme } from "./SwitchTheme";
 
 type HeaderMenuLink = {
   label: string;
@@ -58,41 +58,13 @@ export const HeaderMenuLinks = () => {
  * Site header
  */
 export const Header = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const burgerMenuRef = useRef<HTMLDivElement>(null);
-  useOutsideClick(
-    burgerMenuRef,
-    useCallback(() => setIsDrawerOpen(false), []),
-  );
-
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
-      <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
-          <label
-            tabIndex={0}
-            className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
-            onClick={() => {
-              setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
-            }}
-          >
-            <Bars3Icon className="h-1/2" />
-          </label>
-          {isDrawerOpen && (
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-              onClick={() => {
-                setIsDrawerOpen(false);
-              }}
-            >
-              <HeaderMenuLinks />
-            </ul>
-          )}
+      <div className="navbar-end flex justify-between flex-grow mr-4">
+        <div className="">
+          <Image src="/opinionX_logo-removebg-preview.png" alt="OpinionX Logo" width={100} height={100} />
         </div>
-      </div>
-      <div className="navbar-end flex-grow mr-4">
-        <SwitchTheme/>
+        {/* <SwitchTheme /> */}
         <RainbowKitCustomConnectButton />
         <FaucetButton />
       </div>
